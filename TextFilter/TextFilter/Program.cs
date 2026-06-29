2ec971d92aea1ad6644fbf40bcd7b1d468668dc3
unknown <mcarlisle81@hotmail.co.uk>
2026-06-30 00:02:54 +0100

    Read example text from file and output to console

--- TextFilter/TextFilter/Program.cs
+++ TextFilter/TextFilter/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.IO.Pipelines;
+using System.Reflection;
+using System.Reflection.Metadata;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //System.Console.WriteLine("Hello World!");
+
+        var path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "..\\..");
+
+        string text = File.ReadAllText("Document.txt");
+
+        Console.WriteLine(text);
+    }
+}
+
